d0b9d0c37ce175a40e22b7879c05817d3aad9705
Karl Haidinyak <karl@hcubed.com>
2025-03-25 14:10:12 -0700

    Changed the implementation of imageMat to instead take an unsigned int instead of an unsigned char. I also update the test cases to reflect that change

--- src/imageMat.h
+++ src/imageMat.h
@@ -3,7 +3,6 @@
 
 #include <cstring>
 #include <stdexcept>
-#include <array>
 #include <omp.h>
 
 class imageMat {
@@ -13,7 +12,7 @@ class imageMat {
         imageMat(int nRows, int nCols);
         //Use an array of unsigned chars because each pixel in BMP is represented
         //and 3 BGR hex values
-        imageMat(int nRows, int nCols, const unsigned char* input_data);
+        imageMat(int nRows, int nCols, const unsigned int* input_data);
         imageMat(const imageMat& inputMat);
 
         //The destructor
@@ -23,8 +22,8 @@ class imageMat {
         bool resize(int nRows, int nCols);
 
         //Element access functions
-        std::array<unsigned char, 3> get(int row, int col);
-        bool set(int row, int col, unsigned char* rgb_data);
+        unsigned int get(int row, int col);
+        bool set(int row, int col, unsigned int rgb_data);
         int get_rows();
         int get_cols();
 
@@ -53,7 +52,7 @@ class imageMat {
         int get_range_start(int row, int col);
         
     private:
-        unsigned char* m_data;
+        unsigned int* m_data;
         int n_rows, n_cols, n_elements;
         
 
@@ -64,35 +63,35 @@ imageMat::imageMat() {
     n_rows = 0;
     n_cols = 0;
     n_elements = 0;
-    m_data = new unsigned char[n_elements];
+    m_data = new unsigned int[n_elements];
 }
 
 //Constructor that creates an a flattened matrix containing only white pixels
 imageMat::imageMat(int input_rows, int input_cols) {
     n_rows = input_rows;
     n_cols = input_cols;
-    n_elements = n_rows * n_cols * 3;
-    m_data = new unsigned char[n_elements];
-    unsigned char white_pixel[3] = {0xFF, 0xFF, 0xFF};
+    n_elements = n_rows * n_cols;
+    m_data = new unsigned int[n_elements];
+    unsigned int white_pixel = 0xFFFFFF;
 
-    for (int i = 0; i < n_elements - 3; i += 3) {
-        std::memcpy(m_data + i, white_pixel, 3 * sizeof(unsigned char));
+    for (int i = 0; i < n_elements; ++i) {
+        m_data[i] = white_pixel;
     }
 }
 
 //Constructor that creates a flattened matrix containing the input_data
-imageMat::imageMat(int input_rows, int input_cols, const unsigned char* input_data) {
+imageMat::imageMat(int input_rows, int input_cols, const unsigned int* input_data) {
     if (input_data == nullptr) {
         throw std::invalid_argument("The input_data cannot be null!");
     }
 
     n_rows = input_rows;
     n_cols = input_cols;
-    n_elements = n_rows * n_cols * 3;
+    n_elements = n_rows * n_cols;
 
-    m_data = new unsigned char[n_elements];
+    m_data = new unsigned int[n_elements];
 
-    std::memcpy(m_data, input_data, n_elements * sizeof(unsigned char));
+    std::memcpy(m_data, input_data, n_elements * sizeof(unsigned int));
 }
 
 //Copy constructor
@@ -100,7 +99,7 @@ imageMat::imageMat(const imageMat& inputMat) {
     n_rows = inputMat.n_rows;
     n_cols = inputMat.n_cols;
     n_elements = inputMat.n_elements;
-    m_data = new unsigned char[n_elements];
+    m_data = new unsigned int[n_elements];
 
     for (int i = 0; i < n_elements; ++i) {
         m_data[i] = inputMat.m_data[i];
@@ -119,18 +118,18 @@ imageMat::~imageMat() {
 bool imageMat::resize(int r_rows, int r_cols) {
     n_rows = r_rows;
     n_cols = r_cols;
-    n_elements = n_rows * n_cols * 3;
+    n_elements = n_rows * n_cols;
 
     if (m_data != nullptr) {
         delete[] m_data;
     }
 
-    unsigned char white_pixel[3] = {0xFF, 0xFF, 0xFF};
+    unsigned int white_pixel = 0xFFFFFF;
 
-    m_data = new unsigned char[n_elements];
+    m_data = new unsigned int[n_elements];
     if (m_data != nullptr) {
-        for (int i = 0; i < n_elements - 3; i += 3) {
-            std::memcpy(m_data + i, white_pixel, 3 * sizeof(unsigned char));
+        for (int i = 0; i < n_elements; ++i) {
+            m_data[i] = white_pixel;
         }
 
         return true;
@@ -139,23 +138,23 @@ bool imageMat::resize(int r_rows, int r_cols) {
     }
 }
 
-std::array<unsigned char, 3> imageMat::get(int row, int col) {
+unsigned int imageMat::get(int row, int col) {
     int linear_index = get_range_start(row, col);
 
-    std::array<unsigned char, 3> pixel = {0x00, 0x00, 0x00};
+    unsigned int pixel = 0x000000;
 
     if (linear_index >= 0) {
-        std::copy(m_data + linear_index, m_data + linear_index + 3, pixel.begin());
+        pixel = m_data[linear_index];
     }
 
     return pixel;
 }
 
-bool imageMat::set(int row, int col, unsigned char* rgb_data) {
+bool imageMat::set(int row, int col, unsigned int rgb_data) {
     int linear_index = get_range_start(row, col);
 
     if (linear_index >= 0) {
-        std::memcpy(m_data + linear_index, rgb_data, 3 * sizeof(unsigned char));
+        m_data[linear_index] = rgb_data;
         return true;
     }
 
@@ -175,7 +174,7 @@ bool imageMat::operator== (const imageMat& rhs) const {
         return false;
     }
 
-    for (int i = 0; i < this->n_elements * 3; ++i) {
+    for (int i = 0; i < this->n_elements; ++i) {
         if (this->m_data[i] != rhs.m_data[i]) {
             return false;
         }
@@ -191,8 +190,8 @@ imageMat operator+(const imageMat& lhs, const imageMat& rhs) {
 
     int n_rows = lhs.n_rows;
     int n_cols = lhs.n_cols;
-    int n_elements = n_rows * n_cols * 3;
-    unsigned char* temp_results = new unsigned char[n_elements];
+    int n_elements = n_rows * n_cols;
+    unsigned int* temp_results = new unsigned int[n_elements];
 
     for (int i = 0; i < n_elements; ++i) {
         temp_results[i] = lhs.m_data[i] + rhs.m_data[i];
@@ -206,8 +205,8 @@ imageMat operator+(const imageMat& lhs, const imageMat& rhs) {
 imageMat operator+(const unsigned char& lhs, const imageMat& rhs) {
     int n_rows = rhs.n_rows;
     int n_cols = rhs.n_cols;
-    int n_elements = n_rows * n_cols * 3;
-    unsigned char* temp_results = new unsigned char[n_elements];
+    int n_elements = n_rows * n_cols;
+    unsigned int* temp_results = new unsigned int[n_elements];
 
     for (int i = 0; i < n_elements; ++i) {
         temp_results[i] = lhs + rhs.m_data[i];
@@ -221,8 +220,8 @@ imageMat operator+(const unsigned char& lhs, const imageMat& rhs) {
 imageMat operator+(const imageMat& lhs, const unsigned char& rhs) {
     int n_rows = lhs.n_rows;
     int n_cols = lhs.n_cols;
-    int n_elements = n_rows * n_cols * 3;
-    unsigned char* temp_results = new unsigned char[n_elements];
+    int n_elements = n_rows * n_cols;
+    unsigned int* temp_results = new unsigned int[n_elements];
 
     for (int i = 0; i < n_elements; ++i) {
         temp_results [i] = lhs.m_data[i] + rhs;
@@ -244,40 +243,32 @@ imageMat operator*(const imageMat& lhs, const imageMat& rhs) {
         throw std::invalid_argument("Dimension mismatch!");
     }
 
-    unsigned char* temp_result = new unsigned char[lhs.n_rows * rhs.n_cols * 3];
+    unsigned int* temp_result = new unsigned int[lhs.n_rows * rhs.n_cols];
 
     #pragma omp parallel for
     for (int lhs_row = 0; lhs_row < l_rows; ++lhs_row) {
         for (int rhs_col = 0; rhs_col < r_cols; ++rhs_col) {
 
-            unsigned char blue_result = 0x00;
-            unsigned char green_result = 0x00;
-            unsigned char red_result = 0x00;
+            unsigned int color_result = 0x000000;
 
             for (int lhs_col = 0; lhs_col < l_cols; ++lhs_col) {
 
                 //Same liear_indexing algorithm used below
-                int lhs_linear_index = 3 * ((lhs_row * l_cols) + lhs_col);
+                int lhs_linear_index = ((lhs_row * l_cols) + lhs_col);
 
                 //Different linear indexing algorithm is used since the rhs has
                 //the same number of rows as the lhs has columns
-                int rhs_linear_index = 3 * ((lhs_col * r_cols) + rhs_col);
+                int rhs_linear_index = ((lhs_col * r_cols) + rhs_col);
 
-                unsigned char blue_val = (lhs.m_data[lhs_linear_index] * rhs.m_data[rhs_linear_index]);
-                unsigned char green_val = (lhs.m_data[lhs_linear_index + 1] * rhs.m_data[rhs_linear_index + 1]);
-                unsigned char red_val = (lhs.m_data[lhs_linear_index + 2] * rhs.m_data[rhs_linear_index + 2]);
-
-                //Clamps the output of the color results to prevent overflow since 
-                //unsigned chars can only represent 0 - 255
-                blue_result = std::min(255, int(blue_result) + int(blue_val));
-                green_result = std::min(255, int(green_result) + int(green_val));
-                red_result = std::min(255, int(red_result) + int(red_val));
+                unsigned int color_val = (lhs.m_data[lhs_linear_index] * rhs.m_data[rhs_linear_index]);
+                
+                //Clamps the result to the max value of each pixel (white pixel)
+                unsigned int clamp_size = 0xFFFFFF;
+                color_result = std::min(clamp_size, color_val + color_result);
             }
 
-            int result_linear_index = 3 * ((lhs_row * r_cols) + rhs_col);
-                temp_result[result_linear_index] = blue_result;
-                temp_result[result_linear_index + 1] = green_result;
-                temp_result[result_linear_index + 2] = red_result;
+            int result_linear_index = ((lhs_row * r_cols) + rhs_col);
+                temp_result[result_linear_index] = color_result;
         }
     }
 
@@ -289,11 +280,9 @@ imageMat operator*(const imageMat& lhs, const imageMat& rhs) {
 
 
 //Returns the flattened index of the 3D matrix
-//Similar to the formula index = (row * n_cols) + col, but multiplies by 3
-//because each element in the 2D matrix contains 3 elements
 int imageMat::get_range_start(int row, int col) {
     if ((row < n_rows) && (row >= 0) && (col < n_cols) && (col >= 0)) {
-        return 3 * ((row * n_cols) + col);
+        return ((row * n_cols) + col);
     } else {
         return -1;
     }
--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -1,49 +1,66 @@
 #include <gtest/gtest.h>
 #include "../src/imageMat.h"
 
-TEST(Matrix, Multiplication_Simple) {
+TEST(Matrix_Multiplication, Simple) {
     int a_rows = 1;
     int a_cols = 3;
-    unsigned char a[9] = {1,2,1,2,2,1,1,0,1};
+    unsigned int a[3] = {1, 2, 5};
     imageMat a_mat(a_rows, a_cols, a);
-    delete[] a;
 
     int b_rows = 3;
     int b_cols = 1;
-    unsigned char b[9] = {2,2,2,1,1,0,0,0,1};
+    unsigned int b[3] = {10, 20, 1};
     imageMat b_mat(b_rows, b_cols, b);
-    delete[] b;
 
     int e_rows = 1;
     int e_cols = 1;
-    unsigned char e[3] = {4, 6, 3};
+    unsigned int e[1] = {55};
     imageMat e_mat(e_rows, e_cols, e);
-    delete[] e;
 
-    imageMat actual = a_mat * b_mat;
+    imageMat actual_mat = a_mat * b_mat;
 
-    EXPECT_TRUE(e_rows == actual.get_rows() && e_cols == actual.get_cols());
-    EXPECT_TRUE(e_mat == actual);
-};
+    EXPECT_TRUE(e_mat == actual_mat);
+}
 
-TEST(Matrix, Multiplication_Intermediate) {
-    int a_rows = 1;
+TEST(Matrix_Multiplication, Intermediate) {
+    int a_rows = 2;
     int a_cols = 3;
-    unsigned char a[9] = {1, 2, 1, 2, 2, 1, 1, 0, 1};
+    unsigned int a[6] = {1, 2, 5, 10, 0, 20};
     imageMat a_mat(a_rows, a_cols, a);
 
     int b_rows = 3;
     int b_cols = 2;
-    unsigned char b[18] = {2, 2, 2, 10, 2, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 10, 5};
+    unsigned int b[6] = {10, 20, 20, 0, 1, 2};
     imageMat b_mat(b_rows, b_cols, b);
 
-    int e_rows = 1;
+    int e_rows = 2;
     int e_cols = 2;
-    unsigned char e[6] = {4, 6, 3, 11, 4, 6};
+    unsigned int e[4] = {55, 30, 120, 240};
     imageMat e_mat(e_rows, e_cols, e);
 
-    imageMat actual = a_mat * b_mat;
+    imageMat actual_mat = a_mat * b_mat;
 
-    EXPECT_TRUE(e_rows == actual.get_rows() && e_cols == actual.get_cols());
-    EXPECT_TRUE(e_mat == actual);
+    EXPECT_TRUE(e_mat == actual_mat);
+}
+
+TEST(Matrix_Multiplication, Advanced) {
+    int a_rows = 6;
+    int a_cols = 3;
+    unsigned int a[18] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
+    imageMat a_mat(a_rows, a_cols, a);
+
+    int b_rows = 3;
+    int b_cols = 6;
+    unsigned int b[18] = {1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0};
+    imageMat b_mat(b_rows, b_cols, b);
+
+    int e_rows = 6;
+    int e_cols = 6;
+    unsigned int e[36] = {1, 2, 3, 3, 2, 1, 4, 5, 6, 6, 5, 4, 7, 8, 9, 9, 8, 7, 10, 11, 12, 12, 11, 10, 
+        13, 14, 15, 15, 14, 13, 16, 17, 18, 18, 17, 16};
+    imageMat e_mat(e_rows, e_cols, e);
+
+    imageMat actual_mat = a_mat * b_mat;
+
+    EXPECT_TRUE(e_mat == actual_mat);
 }
